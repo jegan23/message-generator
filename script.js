183300bcc70d0abc92a8c268f10ee1c5b0219cd1
jegan23 <jega6064@gmail.com>
2021-03-26 02:04:48 +0800

    The arrays have been added

---
+++ script.js
@@ -0,0 +1,6 @@
+const happyEmotions = ['Evening news is where they begin with ‘Good evening’, and then proceed to tell you why it isn’t.', 'If you think nobody cares if you’re alive, try missing a couple of payments.', 'Money talks but all mine ever says is good-bye.', 'Team work is important; it helps you put the blame on someone else.']
+//console.log(happyJokes);
+const depressedEmotions = ['Someone is always there to help you.', 'You not alone.', 'You are an important person to someone close to you.']
+//console.log(depressedEmotions);
+const angryEmotions = ['Take deep breaths.', 'Express your frustration to someone you care.', 'Defuse anger with humor.']
+//console.log(angryEmotions);
